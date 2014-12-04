2033b49c8443558f3b3dbb3e1b86bdf8915224a3
shuchu <shuchu@shuchus-MacBook-Pro.local>
2014-12-04 15:10:58 -0500

    imporved runCalcDiffusion.m

--- 15PAKDD/runCalcDiffusionMatrix.m
+++ 15PAKDD/runCalcDiffusionMatrix.m
@@ -19,11 +19,19 @@ P = adj_sim*D;
 
 %% graph diffusion
 %% the following code need to be improved.
+%% creat a array of matrix
+AP = zeros(size(P,1),size(P,2),T+1);
+AP(:,:,1) = P;
+for step = 1:T
+    AP(:,:,step) = P*AP(:,:,step-1);
+end
+
+
 for step =1:T
     alpha = 1/(step+1);
-    F = alpha*P^0;
+    F = alpha*AP(:,:,1);
     for j = 1:step
-      F = F + alpha*P^j;
+      F = F + alpha*AP(:,:,step+1);
     end
     fname = sprintf('%s_F%d.mat',matrix{i},step);
     save(fname,'F');
