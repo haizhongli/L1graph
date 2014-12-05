a16e897fc9d73d240595d31a4acd2fdfb84781d5
sickcpt <sickcpt@gmail.com>
2014-12-05 18:26:46 -0500

    add drawing functions at 15PAKDD folder

--- 15PAKDD/sparseGraphProperties.m
+++ 15PAKDD/sparseGraphProperties.m
@@ -1,3 +1,5 @@
+function [avg_deg_cos,avg_deg_gau] = sparseGraphProperties(name,A_cos,A_gau)
+
 %% check graph properties
 
 %%  A is a weighted directed graph 
@@ -16,63 +18,81 @@ in_deg_cos = sum(A_cos_deg,1);
 in_deg_gau = sum(A_gau_deg,1);
 in_deg = [in_deg_cos',in_deg_gau'];
 figure, hist(in_deg);
-title('In degree');
+sname = sprintf('%s,In-degree',name);
+title(sname);
 legend('Cosine','Gaussian');
+export_fig 'indeg' -pdf -transparent;
 
 % outdegree distribution
 out_deg_cos = sum(A_cos_deg,2);
 out_deg_gau = sum(A_gau_deg,2);
 out_deg = [out_deg_cos,out_deg_gau];
 figure, hist(out_deg);
-title('Out degree');
+sname = sprintf('%s,Out-degree',name);
+title(sname);
 legend('Cosine','Gaussian');
+export_fig 'outdeg' -pdf -transparent;
 
 % inWeight distribution
 in_wt_cos = sum(A_cos,1);
 in_wt_gau = sum(A_gau,1);
 in_wt = [in_wt_cos',in_wt_gau'];
 figure, hist(in_wt);
-title('In weight');
+sname = sprintf('%s,In-weight',name);
+title(sname);
 legend('Cosine','Gaussian');
+export_fig 'inWt' -pdf -transparent;
 
 % outWeight distribution
 out_wt_cos = sum(A_cos,2);
 out_wt_gau = sum(A_gau,2);
 out_wt = [out_wt_cos,out_wt_gau];
 figure, hist(out_wt);
-title('Out weight');
+sname = sprintf('%s,Out-weight',name);
+title(sname);
 legend('Cosine','Gaussian');
+export_fig 'outWt' -pdf -transparent;
 
 % indegree Vs outdegree, scatter plot
 %cos
 figure, scatter(in_deg_cos',out_deg_cos, 'filled');
 xlabel('In-degree');
 ylabel('Out-degree');
-title('In-degree Vs Out-degree, Cosine Similarity');
+sname = sprintf('%s,In-degree Vs Out-degree, Cosine Similarity',name);
+title(sname);
+export_fig 'in_out_deg_cos' -pdf -transparent;
 
 %gau
 figure, scatter(in_deg_gau',out_deg_gau, 'filled');
 xlabel('In-degree');
 ylabel('Out-degree');
-title('In-degree Vs Out-degree, Gaussian Similarity');
+sname = sprintf('%s,In-degree Vs Out-degree, Gaussian Similarity',name);
+title(sname);
+export_fig 'in_out_deg_gau' -pdf -transparent;
 
 % inWeight Vs outWeight, scatter plot
 %cos
 figure, scatter(in_wt_cos',out_wt_cos,'filled');
 xlabel('In-weight');
 ylabel('Out-weight');
-title('In-weight Vs Out-weight, Cosine Similarity');
+sname = sprintf('%s,In-weight Vs Out-weight, Cosine Similarity',name);
+title(sname);
+export_fig 'in_out_wt_cos' -pdf -transparent;
 
 %gau
 figure, scatter(in_wt_gau',out_wt_gau,'filled');
 xlabel('In-weight');
 ylabel('Out-weight');
-title('In-weight Vs Out-weight, Gaussian Similarity');
+sname = sprintf('%s,In-weight Vs Out-weight, Gaussian Similarity',name);
+title(sname);
+export_fig 'in_out_wt_gau' -pdf -transparent;
 
 % averge degree
 avg_deg_cos = sum(sum(A_cos))/n;
 avg_deg_gau = sum(sum(A_gau))/n;
 
-set(gca,'FontSize',26,'fontWeight','bold')
-set(findall(gcf,'type','text'),'FontSize',30,'fontWeight','bold');
+%set(gca,'FontSize',26,'fontWeight','bold')
+%set(findall(gcf,'type','text'),'FontSize',30,'fontWeight','bold');
+
+end
 
--- testDiffusion.m
+++ testDiffusion.m
@@ -6,7 +6,7 @@ true_labels = ClusterLabels;
 n = size(data,1); % number of observations;
 m = size(data,2); % number of features;
 
-sigma = 0.7; %% paramter for guassian kernel.
+
 step = 1;    %% step of random walk;
 minmum_dict_size = m + 1; %% threshold of dictionary
 k_target = m;
@@ -20,14 +20,15 @@ dict_sizes = zeros(n,1);
 
 %% normalization, first feature, then observation (required by L1 graph)
 data = NMRow(data')';
-data = NMRow(data);
+%data = NMRow(data);
 %n = size(data,1); % number of observations;
 %m = size(data,2); % number of features;
 
 %% build the dimilarity matrix;
-adj_euclid = squareform(pdist(data));
-adj_sim = 1 ./ adj_euclid;
-adj_sim(~isfinite(adj_sim)) = 0; %% if two points are duplicated, there are zero similarity
+% adj_euclid = squareform(pdist(data));
+% adj_sim = 1 ./ adj_euclid;
+% adj_sim(~isfinite(adj_sim)) = 0; %% if two points are duplicated, there are zero similarity
+adj_sim = myGaussianMedian(data);
 D = diag ( 1 ./ sum(adj_sim,1));
 
 %% random walk matrix 
@@ -42,9 +43,11 @@ P = adj_sim*D;
 
 %% debug
 if debug_flag
+    H = zeros(50,1);
+    
     d1 = data;
     dif_nbs = zeros(n,1);
-    for i = 1:1:10
+    for i = 1:1:30
         alpha = 1/(i+1);
         F = alpha*P^0;
         for j = 1:i
@@ -52,20 +55,25 @@ if debug_flag
         end
         
         %%draw diffusion
-        
+        H(i) = F(1,1);
+        F(logical(eye(n))) = 0;
         figure, scatter(d1(:,1),d1(:,2),30,F(:,1));
+        title(int2str(i));
         hold on; 
         plot(d1(1,1),d1(1,2),'gd');
-        nonzero_ids = find(F(:,1)>0.001);
+        tmpF = F(:,1);
+        tmpM = (sum(tmpF) - F(1,1)) / (n-1);
+        nonzero_ids = find(F(:,1)>tmpM);
+        setdiff(nonzero_ids,1);
         plot(d1(nonzero_ids,1),d1(nonzero_ids,2),'r+');
-        F(1,1)
-        figure, plot(sort(F(:,1),'descend'));
+        %F(1,1)
+        %figure, plot(sort(F(:,1),'descend'));
         
         %%draw number of diffusion neighbors
-        for j = 1:n
-            dif_nbs(j) = length(find(F(:,j) >0.001));
-        end
-        figure, hist(dif_nbs);
+        %for j = 1:n
+        %    dif_nbs(j) = length(find( F(:,j) > mean(F(:,j))) );
+        %end
+        %figure, hist(dif_nbs);
         
         pause
     end
